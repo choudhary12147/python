ee229601a98a0fabd45f959710461ccdecbc5cdb
sunil dhayal <sunildhayal368@gmail.com>
2025-03-02 10:42:21 +0530

    day_01 of pyhton

---
+++ first.py
@@ -0,0 +1,83 @@
+print("hello world")
+
+#variable and type conversion
+
+# x = 23
+# y = 45
+
+# z = int("34")
+
+# print(z,x,y)
+
+
+"""  list like  array in javascript  """
+
+
+# number = [3,4,1,2]
+
+# print(number)
+
+# number.append(6);
+# number.remove(4);
+# number.reverse();
+# print(number)
+# print(number[-3]);
+
+#input
+
+# user_input = input("enter the name: ")
+
+# print(user_input)
+
+
+#Tuples (immutable list)
+
+# num = (10,20)
+
+# num = (10,20,30,40)
+
+# print(num)
+
+#set (unique values)
+
+# value = {2,2,1,4,1}
+
+# value.add(8)
+# value.remove(2)
+# value.pop()
+# value.clear()
+
+# value.discard(1)
+# value.update(0,2)
+# value.difference_update(4)
+# print(value)
+
+
+#data type and variable in python
+
+# name = "sunil"
+# age = 23
+# marks = 45.89
+# condition = True
+# a = None
+
+# print(type(name),type(age),type(marks),type(condition),type(a))
+
+
+
+
+# operator and operations
+
+# a = 10
+# b= 2
+
+# print((not False ) or  (a>2 and b< 5))
+
+
+#type conversion
+
+val  = int(input("enter the value int"))
+print(val,type(val))
+
+val1 = float(input("enter the value float"))
+print(val1,type(val1))
